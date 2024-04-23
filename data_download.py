ed898954b54edc5826ed84d1f9acc30428903eb9
Ayush Mishra <i.ayushmishra7@gmail.com>
2024-04-23 15:51:19 -0400

    Add files via upload

--- data_download.py
+++ data_download.py
@@ -1,5 +1,5 @@
 '''
-The below code is used to download the data for Mediteranean Sea from year 1991 to 2024.
+The below code is used to download the data for Mediterranean Sea from year 1991 to 2024.
 A user account needs to be created https://www.copernicus.eu/en to use the below code.
 '''
 import copernicusmarine
@@ -8,7 +8,7 @@ import copernicusmarine
 username = 'username'
 password = 'password'
 
-year_list = list(range(1991,2025,1))
+year_list = list(range(2022,2025,1))
 
 # typical file format
 # 19920106000000-GOS-L4_GHRSST-SSTfnd-OISST_HR_REP-MED-v02.0-fv03.0.nc
@@ -21,7 +21,7 @@ for i in year_list:
         # filter = f"{i}010100000*.nc",
         filter = f"{i}*.nc",
         no_directories = True,
-        output_directory = f"data/",
+        output_directory = f"data/{i}",
         username= username,
         password=password,
         force_download = True
